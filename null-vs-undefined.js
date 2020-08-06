3a28e14a303bb0c757db10fee80d852c6f23c789
tareqazeez1 <tareqazeez1@gmail.com>
2020-08-07 01:41:24 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+// const name;
+// console.log(name);
+// Output ==> undefined
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+const result = add(10,20);
+console.log(result);
+
+//Output ==> undefined..... (no return)
+
+
+const result2 = add(10);
+console.log(result2);
+
+//Output ==> undefined...
+
+const love = {name:"Tareq Aziz", phone:123456};
+console.log(love.address);
+
+//Output ==> undefined...   (no value found)
+
+let defination = undefined;
+console.log(defination);
+
+// Output ==> undefined.
+
+
+
+// numm = not existed
+
+// null = When some thing is empty or gonna be empty.
